7f608e335ac92a8ac1af50a9009682bcb3dd9a65
alex4rks <alex4rks@gmail.com>
2017-12-25 00:24:58 +0300

    Delete install.bat

--- Silent Install/DIALux/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing Dialux 4.12!
-
-:: Disable CMD UNC Check
-setlocal enableextensions
-Reg Query "HKLM\SOFTWARE\Microsoft\Command Processor" /v DisableUNCCheck 2>nul | findstr /c:"DisableUNCCheck    REG_DWORD    0x1" >nul
-if %errorlevel%==0 (echo ...UNC is allowed) else (echo +++UNC Support added! & Reg.exe add "HKLM\SOFTWARE\Microsoft\Command Processor" /v "DisableUNCCheck" /t REG_DWORD /d "1" /f >nul)
-
-"%~dp0setup.exe" /s:%~dp0config.ini
